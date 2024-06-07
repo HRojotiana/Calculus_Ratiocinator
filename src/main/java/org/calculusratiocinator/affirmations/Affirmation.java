1e75c3c0280080ce6d35588792886b25504928f8
HRojotiana <hei.rojotiana@gmail.com>
2024-06-07 14:46:11 +0300

    feat: implement Affirmation

---
+++ src/main/java/org/calculusratiocinator/affirmations/Affirmation.java
@@ -0,0 +1,11 @@
+package org.calculusratiocinator.affirmations;
+
+import org.calculusratiocinator.valeurDeVerite.ValeurDeVerite;
+
+public abstract class Affirmation {
+    ValeurDeVerite valeurDeVerite;
+
+    public ValeurDeVerite getValeurDeVerite() {
+        return valeurDeVerite;
+    }
+}
