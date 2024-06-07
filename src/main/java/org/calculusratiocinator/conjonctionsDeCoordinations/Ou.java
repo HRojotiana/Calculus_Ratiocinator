7368e5b43983b0ccda62fc90ad80ef5236f6d571
HRojotiana <hei.rojotiana@gmail.com>
2024-06-07 14:47:06 +0300

    feat: add main

---
+++ src/main/java/org/calculusratiocinator/conjonctionsDeCoordinations/Ou.java
@@ -0,0 +1,11 @@
+package org.calculusratiocinator.conjonctionsDeCoordinations;
+
+import org.calculusratiocinator.affirmations.Affirmation;
+import org.calculusratiocinator.valeurDeVerite.ValeurDeVerite;
+
+public final class Ou extends ConjonctionsDeCoordinations {
+    @Override
+    public ValeurDeVerite calculerValeurDeVeriteDeLaCompositionDeDeuxAffirmations(Affirmation affirmation1, Affirmation affirmation2) {
+        return null;
+    }
+}
