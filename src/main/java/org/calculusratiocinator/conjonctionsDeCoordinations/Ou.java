ccf8bc4d11a457af8062931850c2f70e5b6f1dd2
HRojotiana <hei.rojotiana@gmail.com>
2024-06-07 14:55:26 +0300

    chore: implement Or ConjonctionDeCoordination

--- src/main/java/org/calculusratiocinator/conjonctionsDeCoordinations/Ou.java
+++ src/main/java/org/calculusratiocinator/conjonctionsDeCoordinations/Ou.java
@@ -1,11 +1,20 @@
 package org.calculusratiocinator.conjonctionsDeCoordinations;
 
 import org.calculusratiocinator.affirmations.Affirmation;
+import org.calculusratiocinator.valeurDeVerite.Faux;
 import org.calculusratiocinator.valeurDeVerite.ValeurDeVerite;
+import org.calculusratiocinator.valeurDeVerite.Vrai;
 
 public final class Ou extends ConjonctionsDeCoordinations {
     @Override
     public ValeurDeVerite calculerValeurDeVeriteDeLaCompositionDeDeuxAffirmations(Affirmation affirmation1, Affirmation affirmation2) {
-        return null;
+        Vrai vrai = new Vrai();
+        Faux faux = new Faux();
+        if(affirmation1.getValeurDeVerite() == vrai || affirmation2.getValeurDeVerite() == vrai){
+            return vrai;
+        }
+        else {
+            return faux;
+        }
     }
 }
