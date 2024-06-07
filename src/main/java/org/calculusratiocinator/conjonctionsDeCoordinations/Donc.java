de3ee995c31ace1af528f91450b57f208f5c63af
HRojotiana <hei.rojotiana@gmail.com>
2024-06-07 14:59:36 +0300

    chore: implement Donc ConjonctionDeCoordination

--- src/main/java/org/calculusratiocinator/conjonctionsDeCoordinations/Donc.java
+++ src/main/java/org/calculusratiocinator/conjonctionsDeCoordinations/Donc.java
@@ -1,12 +1,20 @@
 package org.calculusratiocinator.conjonctionsDeCoordinations;
 
 import org.calculusratiocinator.affirmations.Affirmation;
+import org.calculusratiocinator.valeurDeVerite.Faux;
 import org.calculusratiocinator.valeurDeVerite.ValeurDeVerite;
+import org.calculusratiocinator.valeurDeVerite.Vrai;
 
 public final class Donc extends ConjonctionsDeCoordinations {
 
     @Override
     public ValeurDeVerite calculerValeurDeVeriteDeLaCompositionDeDeuxAffirmations(Affirmation affirmation1, Affirmation affirmation2) {
-        return null;
+        Vrai vrai = new Vrai();
+        Faux faux = new Faux();
+        if(affirmation1.getValeurDeVerite() == vrai && affirmation2.getValeurDeVerite() == faux){
+            return faux;
+        }else{
+            return vrai;
+        }
     }
 }
