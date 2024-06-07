099968aedbe6e3aadf5866b8f48f310d0350fbfe
HRojotiana <hei.rojotiana@gmail.com>
2024-06-07 15:07:11 +0300

    feat: implement Verite & add contructor to Affirmation

--- src/main/java/org/calculusratiocinator/affirmations/Affirmation.java
+++ src/main/java/org/calculusratiocinator/affirmations/Affirmation.java
@@ -5,6 +5,10 @@ import org.calculusratiocinator.valeurDeVerite.ValeurDeVerite;
 public abstract class Affirmation {
     ValeurDeVerite valeurDeVerite;
 
+    public Affirmation(ValeurDeVerite valeurDeVerite) {
+        this.valeurDeVerite = valeurDeVerite;
+    }
+
     public ValeurDeVerite getValeurDeVerite() {
         return valeurDeVerite;
     }
---
+++ src/main/java/org/calculusratiocinator/affirmations/Verite.java
@@ -0,0 +1,10 @@
+package org.calculusratiocinator.affirmations;
+
+import org.calculusratiocinator.valeurDeVerite.ValeurDeVerite;
+import org.calculusratiocinator.valeurDeVerite.Vrai;
+
+public class Verite extends Affirmation{
+    public Verite(Vrai vrai) {
+        super(vrai);
+    }
+}
